520c5318159fee2d18194a90ae0632fe5adaa2d0
Philip Poots <philip.poots@gmail.com>
2018-01-29 19:50:48 +0100

    feat: Display Bookshelves and Books Dynamically
    Load Books via the BooksAPI then display them on the page
    dynamically, replacing the static content.
    
    Given we know the Bookshelves in use we can hard-code these
    in the component and iterate over them, simplifying the filter
    logic by keeping it all in one place.
    
    An alternative would be to dynamically fetch the unique list of
    bookshelves attached to books and use those instead of a hard-
    coded object/array.

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import ListBookshelves from './ListBookshelves';
 import SearchBooks from './SearchBooks';
-// import * as BooksAPI from './BooksAPI';
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    });
+  };
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBookshelves />
+          <ListBookshelves
+            books={this.state.books}
+          />
         )} />
         <Route path='/search' render={() => (
           <SearchBooks />
         )} />
       </div>
-    )
-  }
+    );
+  };
 };
 
 export default BooksApp;
