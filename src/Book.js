29d52025c8490f1d47f89b2d8a97e8fcd770485d
Philip Poots <philip.poots@gmail.com>
2018-01-29 20:12:30 +0100

    refactor: Extract shelf-chooser into own component
    Prepare a separate component for the shelf-chooser select box
    with a view to making it a controlled component.

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import BookShelfChanger from './BookShelfChanger';
 
 class Book extends Component {
   render = () => {
@@ -15,15 +16,7 @@ class Book extends Component {
                 height: 193,
                 backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}>
             </div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+            <BookShelfChanger book={book} />
           </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">{book.authors.join(", ")}</div>
